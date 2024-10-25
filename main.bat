895a16e0bd3f2a664065696300f81291474566cc
installtekz <installtekz0@gmail.com>
2024-10-26 00:05:51 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -6,7 +6,7 @@ set /p ip="Enter IP Address: "
 set /p user="Enter Username: "
 set /p wordlist="Enter Password List: "
 
-set /a count=1
+set /a count=0
 for /f %%a in (%wordlist%) do (
   set pass=%%a
   call :attempt
